4ddfde9e2733e73d3966110572d9ff7fec3cce45
Shamee <nachozzzz224@gmail.com>
2023-11-08 23:22:20 +0300

    установил новый фон с градиентом

--- myCardHolder/App.js
+++ myCardHolder/App.js
@@ -7,15 +7,7 @@ import { useEffect, useRef, useState } from 'react';
 import * as MediaLibrary from 'expo-media-library'
 import BackButton from './components/backButton';
 import SnapButton from './components/snapButton';
-
-
-const MainContainer = styled.View`
-flex: 1;
-background-color: '#7B68EE';
-align-items: center;
-justify-content: flex-start;
-flex-direction: column;
-`;
+import { LinearGradient } from 'expo-linear-gradient';
 
 const Qdrat = styled.View`
   width: 100%;
@@ -26,11 +18,23 @@ const Qdrat = styled.View`
   border-bottom-color: black;
 `
 
+const Kdrat = styled.View`
+  width: 100%;
+  height: 30.55%;
+  align-items: center;
+  justify-content: center;
+  padding: 15px 15px 25px 15px;
+  border-bottom-width: 1px;
+  border-bottom-color: black;
+  background-color: red;
+`
+
 const ButtonContainer = styled.View(props => ({
   width: '100%',
   background: props. $biba,
   alignItems: props. $positionItem,
 }));
+
 
 export default function App() {
 
@@ -65,29 +69,36 @@ export default function App() {
   
 
   return (
-    <MainContainer contentPosition={cameraStatus ? 'center' : 'flex-start'} style={styles.container}>      
-    {
-      cameraStatus ? 
-      <Camera style={styles.camera} ref={cameraRef}  ratio={'16:9'}>
-        <ButtonContainer $positionItem='flex-start'>
-          <BackButton ccc={onPressCameraStatus}/>
-        </ButtonContainer>
-        <ButtonContainer $positionItem='center'>
-          <SnapButton  />
-        </ButtonContainer>
-    </Camera> :
-  <Qdrat>
-    <AddButton cameraSet={onPressCameraStatus}/>
-  </Qdrat> 
-    }     
-  </MainContainer>
+      <LinearGradient colors={[ '#FFFFE0',  '#FFFACD', '#DCDCDC']} /* locations={[0.4, 1, 0.1]} */ /* start={{x: 0, y: 1}} end={{x: 0, y: 0.7}} */  style={styles.container}>
+          {
+            cameraStatus ? 
+            <Camera style={styles.camera} ref={cameraRef}  ratio={'16:9'}>
+              <ButtonContainer $positionItem='flex-start'>
+                <BackButton ccc={onPressCameraStatus}/>
+              </ButtonContainer>
+              <ButtonContainer $positionItem='center'>
+                <SnapButton  />
+              </ButtonContainer>
+          </Camera> :
+        <Qdrat>
+          <AddButton cameraSet={onPressCameraStatus}/>
+        </Qdrat> 
+  
+    }
+
+    <Kdrat>
+    {/*   <View style={{width: '40px', height: '50%', backgroundColor: 'black'}}></View> */}
+     <Image source={require('./components/1270001.svg')} style={{width: '20%' ,height: '20%'}}></Image>
+    </Kdrat>
+   
+    
+      </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#7B68EE',
     alignItems: 'center',
     justifyContent: 'flex-start',
     flexDirection: 'column',
